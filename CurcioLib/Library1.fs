bd61d667f6d6f035609395e2ff0ed6437392e94e
Guido <scatena.guido@gmail.com>
2022-01-11 21:27:02 +0000

    rev

--- CurcioLib/Library1.fs
+++ CurcioLib/Library1.fs
@@ -13,6 +13,15 @@ let parse_antigen_string (input:string) =
     done
     antigens, values
 
+let parse_antigen_parts_string (input:string) = 
+    let mutable antigens = List.empty
+    let temp = input.Split(' ')
+    for t in temp do
+        let tt = t.Split('(',')')
+        antigens <- (tt.[0],tt.[1]) :: antigens
+    done
+    antigens
+
 [<ExcelFunction(Description="Return antigens that are in the minuend but not in subtrahend")>]
 let AntigenDifference (minuend:string) (subtrahend:string) = 
     let antigen_minuend, values_minuend = parse_antigen_string minuend
@@ -22,7 +31,9 @@ let AntigenDifference (minuend:string) (subtrahend:string) =
     System.String.Join(" ",  antigen_difference) 
 
 [<ExcelFunction(Description="Return sum of antigen values in input string")>]
-let AntigenValueSum (input:string)  = 
-    let antigen_input, values_input = parse_antigen_string input
-    Seq.map (fun (a:string) -> System.Int32.Parse(a)) values_input
+let AntigenValueSum (input:string, hla_type:string)  = 
+    let antigen_input = parse_antigen_parts_string input
+    antigen_input
+        |> Seq.filter (fun (t:string, v:string) -> t.Contains(hla_type)) 
+        |> Seq.map (fun (t, v:string) -> System.Int32.Parse(v)) 
         |> Seq.fold (fun (acc:int) (a:int) -> acc + a) 0 
